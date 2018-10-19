e977bac67c301ae68af13b5f16feb46d6b5c34af
YangXuepei <1155107976@link.cuhk.edu.hk>
2018-10-19 14:17:39 +0800

    naive effort function

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import basic as b
 import read_code_and_testdata as r
 
 
-#code_dir = r.readfile("testdata.csv")
-#test_data = r.load_test("test.txt")
+code_dir = r.readfile("testdata.csv")
+test_data = r.load_test("test.txt")
 
-code_dir = r.readfile("single.csv")
-test_data = r.load_test("beiying.txt") # 测试数据：朱自清散文《背影》
+#code_dir = r.readfile("single.csv")
+#test_data = r.load_test("beiying.txt") # 测试数据：朱自清散文《背影》
 eval_type = None
 while eval_type != "q":
     if eval_type is not None:
